9511ecdb584851a00711fe0707667977c5dbb1a9
delaitaris <tomdawarrior@gmail.com>
2026-09-04 22:27:52 +1200

    got the sprites changing
    next job is to change the speed of moving depending on what stance you're in, which is proving harder than you'd think

---
+++ scenes/animated_sprite_2d.gd
@@ -0,0 +1,27 @@
+extends AnimatedSprite2D
+enum Stance {STANDING, CROUCHING, CRAWLING}
+var current_stance = Stance.STANDING
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play("default")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("lower height"):
+		match current_stance:
+			Stance.STANDING: 
+				current_stance = Stance.CROUCHING
+				play("crouch")
+			Stance.CROUCHING:
+				current_stance = Stance.CRAWLING
+				play("crawl")
+	if Input.is_action_just_pressed("raise height"):
+		match current_stance:
+			Stance.CROUCHING: 
+				current_stance = Stance.STANDING
+				play("default")
+			Stance.CRAWLING:
+				current_stance = Stance.CROUCHING
+				play("crouch")
