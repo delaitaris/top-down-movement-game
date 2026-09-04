f89c14ae5581fede8206d4a6d2e766cfdefcc0ea
delaitaris <tomdawarrior@gmail.com>
2026-09-04 14:13:43 +1200

    culled shooting for a bit

--- character.gd
+++ character.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
 @onready var sprite: Sprite2D = $"Sprite2D"
-@onready var muzzle: Marker2D = $Muzzle
-@export var projectile: PackedScene
 
 
 const SPEED = 300
@@ -10,8 +8,6 @@ const SPEED = 300
 func _process(delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 	sprite.look_at(mouse_pos)
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
 
 #movement
 func _physics_process(delta: float) -> void:
@@ -31,10 +27,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if Input.is_action_just_released("d"):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-func shoot() -> void:
-	projectile.global_position = muzzle.global_position
-	projectile.direction = Vector2.RIGHT.rotated(global_rotation)
-	projectile.rotation = global_rotation
 
 	move_and_slide()
