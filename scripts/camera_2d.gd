354a7d487b0fdb2dd52fdb807b681192425a0c2d
delaitaris <tomdawarrior@gmail.com>
2026-09-04 19:03:14 +1200

    made it work
    players now move faster when moving in the same direction they're looking. i might make it a bit less jarring; aka lowering boost speed a bit or raising base speed

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,18 +1,16 @@
 extends Camera2D
 @export var zoom_speed: float = 0.1
-@export var min_zoom: float = 0.5
+@export var min_zoom: float = 0.7
 @export var max_zoom: float = 3.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _unhandled_input(event: InputEvent) -> void:
+	#zoom in and out with scroll wheel
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			zoom -= Vector2(zoom_speed, zoom_speed)
---
+++ scripts/character.gd
@@ -0,0 +1,65 @@
+extends CharacterBody2D
+@onready var sprite: Sprite2D = $"Sprite2D"
+@export_range(0.0, 180.0) var cone_angle_degrees := 60.0
+@export var normal_speed: float = 150.0
+@export var boosted_speed: float = 300.0
+@onready var Poles: Area2D = $"Poles"
+
+var north = false
+var south = false
+var east = false
+var west = false
+
+#mouse look & shooting
+func _process(delta: float) -> void:
+	var mouse_pos = get_global_mouse_position()
+	sprite.look_at(mouse_pos)
+
+#movement
+func _physics_process(delta: float) -> void:
+	var mouse_direction = global_position.direction_to(get_global_mouse_position())
+	var movement_direction := Input.get_vector("a","d", "w", "s").normalized()
+	var speed: float = normal_speed
+	
+	if movement_direction != Vector2.ZERO:
+			if north and velocity.y < -100:
+				speed = boosted_speed
+			if south and velocity.y > 100:
+				speed = boosted_speed
+			if east and velocity.x > 100:
+				speed = boosted_speed
+			if west and velocity.x < -100:
+				speed = boosted_speed
+
+	velocity = movement_direction * speed
+	print(velocity)
+	move_and_slide()
+
+
+# POLES CHECKING
+
+
+#north
+func _on_north_mouse_entered() -> void:
+	print("north")
+	north = true
+func _on_north_mouse_exited() -> void:
+	north = false
+#south
+func _on_south_mouse_entered() -> void:
+	print("south")
+	south = true
+func _on_south_mouse_exited() -> void:
+	south = false
+#west
+func _on_west_mouse_entered() -> void:
+	print("west")
+	west = true
+func _on_west_mouse_exited() -> void:
+	west = false
+#east
+func _on_east_mouse_entered() -> void:
+	print("east")
+	east = true
+func _on_east_mouse_exited() -> void:
+	east = false
