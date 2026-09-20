e1b7ec1478a7b47aff7c62467a17095ba3e777b2
delait <tomdawarrior@gmail.com>
2026-09-21 09:39:11 +1200

    added in gun states
    you can switch between them. for some fuckass reason, when you use the rifle, it only shoots when your mouse is moving??????? what???????? i don't know, this shit is wack bro. atleast i can code new guns and pickup things much, much easier now. and yes, coding in main branch, i know

--- scripts/character.gd
+++ scripts/character.gd
@@ -18,7 +18,7 @@ extends CharacterBody2D
 @export_range(0.0, 180.0) var cone_angle_degrees := 60.0
 @export var bullet_scene: PackedScene
 
-
+enum Gun {PISTOL, RIFLE, SHOTGUN, UNARMED}
 enum Stance {STANDING, CROUCHING, CRAWLING}
 
 var north = false
@@ -26,13 +26,14 @@ var south = false
 var east = false
 var west = false
 var current_stance = Stance.STANDING
-var pistolmag = 7
 var shootcooldown = false
 var reloading = false
+var cooldowntime = 1
+var current_gun = Gun.PISTOL
+var magazine = 1
 
 func _ready() -> void:
 	muzzleflash.shadow_enabled = true
-
 
 #mouse look & shooting
 func _process(_delta: float) -> void:
@@ -42,6 +43,13 @@ func _process(_delta: float) -> void:
 	sprite.look_at(mouse_pos)
 	muzzle.look_at(mouse_pos)
 	muzzleblock.look_at(mouse_pos)
+
+	if current_gun == Gun.PISTOL:
+		cooldowntime = 0.2
+	if current_gun == Gun.RIFLE:
+		cooldowntime = 0.1
+	if current_gun == Gun.SHOTGUN:
+		cooldowntime = 0.8
 
 #movement
 func _physics_process(_delta: float) -> void:
@@ -118,31 +126,65 @@ func _on_east_mouse_exited() -> void:
 
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("shoot") and !shootcooldown and !reloading:
+		if current_gun == Gun.PISTOL:
 			shoot()
-			pistolmag -= 1
-			if pistolmag <= 0:
-				pistolmag = 0
-			pistolshootcooldown.start()
+			magazine -= 1
+			if magazine <= 0:
+				magazine = 0
+			pistolshootcooldown.start(cooldowntime)
 			shootcooldown = true
+		if current_gun == Gun.SHOTGUN:
+			shoot()
+			magazine -= 1
+			if magazine <= 0:
+				magazine = 0
+			pistolshootcooldown.start(cooldowntime)
+			shootcooldown = true
+	elif Input.is_action_pressed("shoot") and !shootcooldown and !reloading and current_gun == Gun.RIFLE:
+		shoot()
+		magazine -= 1
+		if magazine <= 0:
+			magazine = 0
+		pistolshootcooldown.start(cooldowntime)
+		shootcooldown = true
+	else:
+		pass
+
 	if Input.is_action_just_pressed("reload"):
 		reload()
 	if Input.is_action_just_pressed("flashlight"):
 		flashlight()
+	
+	
+	if Input.is_action_just_pressed("1"):
+		current_gun = Gun.UNARMED
+		magazine = 0
+	elif Input.is_action_just_pressed("2"):
+		current_gun = Gun.PISTOL
+		magazine = 7
+	elif Input.is_action_just_pressed("3"):
+		current_gun = Gun.RIFLE
+		magazine = 30
+	elif Input.is_action_just_pressed("4"):
+		current_gun = Gun.SHOTGUN
+		magazine = 5
+
 
 func shoot() -> void:
 	var bullet = bullet_scene.instantiate()
-	
 	$Muzzleflash.texture_scale = randf_range(4, 6)
 	$Muzzleflash.energy = randf_range(1.5, 2.5)
-	if pistolmag != 0:
+	
+	if magazine != 0:
 		muzzleflash.enabled = true
 		timerflash.start()
 	if not bullet_scene:
 		return
 	bullet.global_position = muzzle.global_position
 	bullet.global_rotation = muzzle.global_rotation
-	if pistolmag != 0:
+	if magazine != 0:
 		get_tree().current_scene.add_child(bullet)
+
 
 func flashlight() -> void:
 	if flashlight_toggle.enabled == false:
@@ -151,11 +193,24 @@ func flashlight() -> void:
 		flashlight_toggle.enabled = false
 
 func reload() -> void:
-	if pistolmag < 7:
-		reloading = true
-		reloadtimer.start()
-		if pistolreloadsfx.playing == false:
-			pistolreloadsfx.play()
+	if current_gun == Gun.PISTOL:
+		if magazine < 7:
+			reloading = true
+			reloadtimer.start()
+			if pistolreloadsfx.playing == false:
+				pistolreloadsfx.play()
+	if current_gun == Gun.RIFLE:
+		if magazine < 30:
+			reloading = true
+			reloadtimer.start()
+			if pistolreloadsfx.playing == false:
+				pistolreloadsfx.play()
+	if current_gun == Gun.SHOTGUN:
+		if magazine < 5:
+			reloading = true
+			reloadtimer.start()
+			if pistolreloadsfx.playing == false:
+				pistolreloadsfx.play()
 	else:
 		return
 
@@ -168,7 +223,12 @@ func _on_light_darkener_timeout() -> void:
 	lightdarkencanvas.color = Color(0.161, 0.161, 0.153, 1.0)
 
 func _on_reload_timeout() -> void:
-	pistolmag = 7
+	if current_gun == Gun.PISTOL:
+		magazine = 7
+	if current_gun == Gun.RIFLE:
+		magazine = 30
+	if current_gun == Gun.SHOTGUN:
+		magazine = 5
 	reloading = false
 
 func _on_shoot_cooldown_timeout() -> void:
